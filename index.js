732f214746e5d84e3842e1b3353a7e42310ef452
Brunno <brunnopereira090@gmail.com>
2023-04-05 11:58:35 -0300

    Primeira versão do sistema servidor

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const hostname = 'localhost'
+const PORT = 3000
+/*--------configurações do express*/
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json())
+/*--------------------------------------------*/
+
+/* ----- arquivoes estáticos ---------------*/
+app.use(express.static('public'))
+/*--------------------------------------------*/
+
+/* ------ Rotas do projeto -----------------*/
+
+app.get('/',(req,res)=>{
+    res.render('index.html')
+})
+
+/*--------------------------------------------*/
+app.listen(PORT,hostname, ()=>{
+    console.log(`O servidor está rodando ${hostname}:${PORT}`)
+})
